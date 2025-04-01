fcc93ce09fd60da6b0d028fa107434608284d782
mack-hendrik <mackhendrik304@gmail.com>
2025-04-01 15:35:37 +0700

    chore: update configuration files and add new dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
